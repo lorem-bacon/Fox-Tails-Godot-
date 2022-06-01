61e73fd5412f15e9d9619d7d42736d54b93b16f6
lorem-bacon <spideysony17@gmail.com>
2022-06-01 18:56:04 +0530

    bug fixed: preload() added to deathanimation scripts; bat enemy death effect offset changed

--- Scripts/Grass.gd
+++ Scripts/Grass.gd
@@ -1,7 +1,8 @@
 extends Node2D
 
+var GrassEffect = preload("res://Effects/GrassEffect.tscn")
+
 func create_grass_destroy_effect():
-	var GrassEffect = load("res://Effects/GrassEffect.tscn")
 	var grassEffect = GrassEffect.instance()
 	var world = get_tree().get_current_scene()
 	grassEffect.global_position = global_position
