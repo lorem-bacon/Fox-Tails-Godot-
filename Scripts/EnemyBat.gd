1fd39b5c81100b38fa008386a80bcf28b743b5af
lorem-bacon <spideysony17@gmail.com>
2022-06-01 18:53:12 +0530

    Bat Enemy death effect added; Single script for all death effects

--- Scripts/EnemyBat.gd
+++ Scripts/EnemyBat.gd
@@ -4,6 +4,8 @@ onready var animatedSprite = $AnimatedSprite
 onready var knockBack = Vector2.RIGHT
 onready var stats = $Stats
 
+var BatEnemyDeathEffect = preload("res://Effects/BatEnemyDeathEffect.tscn")
+
 func _ready():
 	animatedSprite.playing = true
 
@@ -14,8 +16,11 @@ func _physics_process(delta):
 func _on_HurtBox_area_entered(area):
 	stats.health -= area.damage
 	knockBack = area.knockBackVector * 120
-#	queue_free()
 
 
 func _on_Stats_no_health():
+	var batEnemyDeathEffect = BatEnemyDeathEffect.instance()
+	batEnemyDeathEffect.global_position = global_position
+	var world = get_tree().current_scene
+	world.add_child(batEnemyDeathEffect)
 	queue_free()
--- Scripts/Stats.gd
+++ Scripts/Stats.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-export(int) var max_health = 2
+export(int) var max_health = 4
 onready var health = max_health setget set_health
 
 
