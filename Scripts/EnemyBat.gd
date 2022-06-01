a30eff90dd58c0e8665b175004ece06477724c97
lorem-bacon <spideysony17@gmail.com>
2022-06-02 00:35:24 +0530

    enemy hit effect added; bug fixing

--- Scripts/EnemyBat.gd
+++ Scripts/EnemyBat.gd
@@ -6,6 +6,7 @@ onready var stats = $Stats
 onready var playerDetectionZone = $PlayerDetectionZone
 
 var BatEnemyDeathEffect = preload("res://Effects/BatEnemyDeathEffect.tscn")
+var EnemyHitEffect = preload("res://Effects/EnemyHitEffect.tscn")
 
 export var ACCELERATION = 300
 export var FRICTION = 300
@@ -39,22 +40,27 @@ func _physics_process(delta):
 
 func _on_HurtBox_area_entered(area):
 	stats.health -= area.damage
+	create_hit_effect()
 	knockBack = area.knockBackVector * 120
 
 
 func _on_Stats_no_health():
 	var batEnemyDeathEffect = BatEnemyDeathEffect.instance()
 	batEnemyDeathEffect.global_position = global_position
-	var world = get_tree().current_scene
-	world.add_child(batEnemyDeathEffect)
+	get_parent().add_child(batEnemyDeathEffect)
 	queue_free()
 
+func create_hit_effect():
+	var enemyHitEffect = EnemyHitEffect.instance()
+	enemyHitEffect.global_position = global_position
+	get_tree().current_scene.add_child(enemyHitEffect)
 
 func chase_player(delta):
 	var player = playerDetectionZone.player
 	if player != null:
 		var enemyMoveDirection = (player.global_position - global_position).normalized()
 		velocity = velocity.move_toward(enemyMoveDirection * MAX_SPEED, ACCELERATION * delta)
+		animatedSprite.flip_h = enemyMoveDirection.x < 0
 	else:
 		enemyState = IDLE
 		
---
+++ Scripts/EnemyHitEffect.gd
@@ -0,0 +1,10 @@
+extends AnimatedSprite
+
+
+func _ready():
+	frame = 0
+	play("Animate")
+	connect("animation_finished", self, "_on_animation_finished")
+	
+func _on_animation_finished():
+	queue_free()
